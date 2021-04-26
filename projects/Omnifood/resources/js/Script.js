f30033eee8ac4887155b8ed355fe5cff524c59f2
fahadprod <fahadahmadmd@gmail.com>
2021-04-26 16:53:33 +0500

    logo and pages updated

---
+++ projects/Omnifood/resources/js/Script.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+    $('.js--section-features').waypoint(function(direction){
+        if (direction == "down"){
+            $('nav').addClass('sticky');//here $ is used as a selection paramater, such as select the nav etc.
+        }
+        else{
+            $('nav').removeClass('sticky');
+        }
+    },{
+        offset:'60px;'
+    });
+/*scroll on buttons*/
+$('.js--scroll-to-plans').click(function(){
+    $('html, body').animate({scrollTop:$('.js--section-plans').offset().top},1000)
+})
+$('.js--scroll-to-start').click(function(){
+    $('html, body').animate({scrollTop:$('.js--section-features').offset().top},1000)
+})
+
+/*navigation scrolling*/
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(){
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{
+        offset:'60%'
+    })
+    $('.js--wp-2').waypoint(function(){
+    $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+        offset:'70%'
+    })
+    $('.js--wp-3').waypoint(function(){
+        $('.js--wp-3').addClass('animated zoomIn');
+    },{
+        offset:'70%'
+    })
+    $('.js--wp-4').waypoint(function(){
+        $('.js--wp-4').addClass('animated pulse');
+    },{
+        offset:'70%'
+    })
+    $('.js--wp-5').waypoint(function(){
+        $('.js--wp-5').addClass('animated fadeInLeft');
+    },{
+        offset:'70%'
+    })
+    /*mobile navigation*/
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon =$('.js--nav-icon i')
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon-round')){
+             icon.addClass('ion-close-round')
+             icon.removeClass('ion-navicon-round')
+        }
+        else{
+            icon.addClass('ion-navicon-round')
+            icon.removeClass('ion-close-round')           
+        }
+    });
+});
---
+++ projects/music/js/main.js
@@ -0,0 +1,200 @@
+// define a variables
+
+let audio, playbtn, title, poster, artists, mutebtn,
+    seekslider, volumeslider, seeking = false, seekto,
+    curtimetext, durtimetext, playlist_status, dir, playlist,
+    ext, agent, playlist_artist, repeat, randomSong;
+
+
+    // initialization of array of music , title poster , image and artist
+
+dir = "music/";
+playlist = ["Cartoon-On", "Elektronomia", "Johnning",
+    "Popsicle", "Fearless"];
+title = ["Cartoon - On", "Elektronomia", "Janji-Heroes Tonight",
+    "Popsicle", "Lost Sky-Fearless"];
+artists = ["(feat. Daniel Levi) [NCS Release]", "Elektronomia-Sky Hight [NCS Release]",
+    "(feat, Johnning) [NCS Release]", "LFZ - [NCS Release]", "(feat. Chris Linton) [NCS Release]"
+];
+
+poster = ["./Images/ncs1.jpeg", "./Images/ncs2.jpg", "./Images/ncs3.jpg",
+    "./Images/ncs4.jpg", "./Images/ncs5.jpg"];
+
+    // Used to run on every browser
+
+ext = ".mp3";
+agent = navigator.userAgent.toLowerCase();
+if (agent.indexOf('firefox') != -1 || agent.indexOf('opera') != -1) {
+    ext = ".ogg";
+}
+
+// set object references
+
+playbtn = document.getElementById("playpausebtn");
+nextbtn = document.getElementById("nextbtn");
+prevbtn = document.getElementById("prevbtn");
+mutebtn = document.getElementById("mutebtn");
+seekslider = document.getElementById("seekslider");
+volumeslider = document.getElementById("volumeslider");
+curtimetext = document.getElementById("curtimetext");
+durtimetext = document.getElementById("durtimetext");
+playlist_status = document.getElementById("playlist_status");
+playlist_artist = document.getElementById("playlist_artist");
+repeat = document.getElementById("repeat");
+randomSong = document.getElementById("random");
+
+playlist_index = 0;
+
+// audio Object
+
+audio = new Audio();
+audio.src = dir + playlist[0] + ext;
+audio.loop = false;
+
+// First song title and artist
+
+playlist_status.innerHTML = title[playlist_index];
+playlist_artist.innerHTML = artists[playlist_index];
+
+//Add Event Handling
+
+playbtn.addEventListener("click", playPause);
+nextbtn.addEventListener("click", nextSong);
+prevbtn.addEventListener("click", prevSong);
+mutebtn.addEventListener("click", mute);
+seekslider.addEventListener("mousedown", function (event) {
+    seeking = true;
+    seekslider(event);
+});
+seekslider.addEventListener("mousemove", function (event) {
+    seek(event);
+});
+seekslider.addEventListener("mouseup", function (event) {
+    seeking = false;
+});
+volumeslider.addEventListener("mousemove", setvolume);
+audio.addEventListener("timeupdate", function () {
+    seektimeupdate();
+});
+audio.addEventListener("ended", function () {
+    switchTrack();
+});
+
+repeat.addEventListener("click", loop);
+randomSong.addEventListener("click", random);
+
+
+function fetchMusicDetails() {
+    $("#playpausebtn img").attr("src", "./Images/pause-red.png");
+    $("#bgImage").attr("src", poster[playlist_index]);
+    $("#image").attr("src", poster[playlist_index]);
+
+    //Title and Artist
+    playlist_status.innerHTML = title[playlist_index];
+    playlist_artist.innerHTML = artists[playlist_index];
+
+    //Audio
+
+    audio.src = dir + playlist[playlist_index] + ext;
+    audio.play();
+}
+
+function playPause() {
+    if (audio.paused) {
+        audio.play();
+        $("#playpausebtn img").attr("src", "./Images/pause-red.png");
+    } else {
+        audio.pause();
+        $("#playpausebtn img").attr("src", "./Images/play-red.png");
+    }
+}
+
+function nextSong() {
+    playlist_index++;
+    if (playlist_index > playlist.length - 1) {
+        playlist_index = 0;
+    }
+    fetchMusicDetails();
+}
+function prevSong() {
+    playlist_index--;
+    if (playlist_index < 0) {
+        playlist_index = playlist.length - 1;
+    }
+    fetchMusicDetails();
+}
+function mute() {
+    if (audio.muted) {
+        audio.muted = false;
+        $("#mutebtn img").attr("src", "./Images/speaker.png");
+    } else {
+        audio.muted = true;
+        $("#mutebtn img").attr("src", "./Images/mute.png");
+    }
+}
+
+function seek(event) {
+    if (audio.duration == 0) {
+        null
+    } else {
+        if (seeking) {
+            seekslider.value = event.clientX - seekslider.offsetLeft;
+            seekto = audio.duration * (seekslider.value / 100);
+            audio.currentTime = seekto;
+        }
+    }
+}
+function setvolume() {
+    audio.volume = volumeslider.value / 100;
+}
+
+
+function seektimeupdate() {
+    if (audio.duration) {
+        let nt = audio.currentTime * (100 / audio.duration);
+        seekslider.value = nt;
+        var curmins = Math.floor(audio.currentTime / 60);
+        var cursecs = Math.floor(audio.currentTime - curmins * 60);
+        var durmins = Math.floor(audio.duration / 60);
+        var dursecs = Math.floor(audio.duration - durmins * 60);
+        if (cursecs < 10) { cursecs = "0" + cursecs }
+        if (dursecs < 10) { dursecs = "0" + dursecs }
+        if (curmins < 10) { curmins = "0" + curmins }
+        // if (dursecs < 10) { dursecs = "0" + dursecs }
+        curtimetext.innerHTML = curmins + ":" + cursecs
+        durtimetext.innerHTML = durmins + ":" + dursecs
+    } else {
+        curtimetext.innerHTML = "00" + ":"+ "00";
+        durtimetext.innerHTML = "00" + ":" + "00";
+    }
+}
+
+function switchTrack() {
+    if (playlist_index == (playlist.length - 1)) {
+        playlist_index = 0;
+    } else {
+        playlist_index++;
+    }
+    fetchMusicDetails();
+}
+function loop() {
+    if (audio.loop) {
+        audio.loop = false;
+        $("#repeat img").attr("src", "./Images/rep.png");
+    } else {
+        audio.loop = true;
+        $("#repeat img").attr("src", "./Images/rep1.png");
+    }
+}
+
+function getRandomNumber(min, max) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+    return result;
+}
+function random() {
+    let randomIndex = getRandomNumber(0, playlist.length - 1);
+    playlist_index = randomIndex;
+    fetchMusicDetails();
+}
